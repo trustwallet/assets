6dd5a871ac43b3d886106100a257939710a9dba6
Mykola <kolya182@gmail.com>
2019-11-07 01:01:26 -0800

    Move helper meethods to sepaarate file for future reuse

---
+++ test/helpers.js
@@ -0,0 +1,44 @@
+
+const fs = require('fs')
+const axios = require('axios')
+const path = require('path')
+const Web3 = require('web3')
+const web3 = new Web3('ws://localhost:8546');
+
+export const BNB = 'binance'
+export const Cosmos = 'cosmos'
+export const Tezos = 'tezos'
+export const TRON = 'tron'
+export const IoTeX = 'iotex'
+export const Waves = 'waves'
+
+export const logo = `logo.png`
+export const chainsFolderPath = './blockchains'
+export const getChainLogoPath = chain => `${chainsFolderPath}/${chain}/info/${logo}`
+export const getChainAssetsPath = chain => `${chainsFolderPath}/${chain}/assets`
+export const getChainAssetLogoPath = (chain, address) => `${getChainAssetsPath(chain)}/${address}/${logo}`
+export const getChainValidatorsPath = chain => `${chainsFolderPath}/${chain}/validators`
+
+export const readDirSync = path => fs.readdirSync(path)
+export const isPathExistsSync = path => fs.existsSync(path)
+export const readFileSync = path => fs.readFileSync(path, (err, data) => {
+    if (err) throw err
+    return data
+}) 
+export const isLowerCase = str => str.toLowerCase() === str
+export const isUpperCase = str => str.toUpperCase() === str
+export const isChecksum = address => web3.utils.checkAddressChecksum(address)
+export const getBinanceTokenSymbols = async () => axios.get(`https://dex-atlantic.binance.org/api/v1/tokens?limit=1000`).then(res => res.data.map(({symbol}) => symbol))
+
+export const isTRC10 = id => (/^\d+$/.test(id))
+export const isTRC20 = address => {
+    return address.length == 34 &&
+    address.startsWith("T") &&
+    isLowerCase(address) == false &&
+    isUpperCase(address) == false
+}
+
+export const getChainValidatorsListPath = chain => `${(getChainValidatorsPath(chain))}/list.json`
+export const getChainValidatorsAssetsPath = chain => `${getChainValidatorsPath(chain)}/assets`
+export const getChainValidatorsAssets = chain => readDirSync(getChainValidatorsAssetsPath(chain))
+export const getChainValidatorAssetLogoPath = (chain, asset) => `${getChainValidatorsAssetsPath(chain)}/${asset}/${logo}`
--- test/index.test.js
+++ test/index.test.js
@@ -1,69 +1,51 @@
-const fs = require('fs')
-const axios = require('axios')
-const path = require('path')
-const Web3 = require('web3')
-const web3 = new Web3('ws://localhost:8546');
 const eztz = require('eztz-lib')
-
-const readDirSync = path => fs.readdirSync(path)
-const isPathExistsSync = path => fs.existsSync(path)
-const readFileSync = path => fs.readFileSync(path, (err, data) => {
-    if (err) throw err
-    return data
-}) 
-const isLowerCase = str => str.toLowerCase() === str
-const isUpperCase = str => str.toUpperCase() === str
-const isChecksum = address => web3.utils.checkAddressChecksum(address)
-const getBinanceTokenSymbols = async () => axios.get(`https://dex-atlantic.binance.org/api/v1/tokens?limit=1000`).then(res => res.data.map(({symbol}) => symbol))
-
-const isTRC10 = id => (/^\d+$/.test(id))
-const isTRC20 = address => {
-    return address.length == 34 &&
-    address.startsWith("T") &&
-    isLowerCase(address) == false &&
-    isUpperCase(address) == false
-}
-
-const logo = `logo.png`
-const BNB = 'binance'
-const Cosmos = 'cosmos'
-const Tezos = 'tezos'
-const TRON = 'tron'
-const IoTeX = 'iotex'
-const Waves = 'waves'
-
-const chainsFolderPath = './blockchains'
-const getChainLogoPath = chain => `${chainsFolderPath}/${chain}/info/${logo}`
-const getChainAssetsPath = chain => `${chainsFolderPath}/${chain}/assets`
-const getChainAssetLogoPath = (chain, address) => `${getChainAssetsPath(chain)}/${address}/${logo}`
-const getChainValidatorsPath = chain => `${chainsFolderPath}/${chain}/validators`
-const getChainValidatorsListPath = chain => `${(getChainValidatorsPath(chain))}/list.json`
-const getChainValidatorsAssetsPath = chain => `${getChainValidatorsPath(chain)}/assets`
-const getChainValidatorsAssets = chain => readDirSync(getChainValidatorsAssetsPath(chain))
-const getChainValidatorAssetLogoPath = (chain, asset) => `${getChainValidatorsAssetsPath(chain)}/${asset}/${logo}`
+import {
+    BNB,
+    Cosmos,
+    Tezos,
+    TRON,
+    IoTeX,
+    Waves,
+    chainsFolderPath,
+    getChainLogoPath,
+    getChainAssetsPath,
+    getChainAssetLogoPath,
+    getChainValidatorsAssets,
+    getChainValidatorsListPath,
+    getChainValidatorAssetLogoPath,
+    readDirSync,
+    isPathExistsSync,
+    readFileSync,
+    isLowerCase,
+    isChecksum,
+    getBinanceTokenSymbols,
+    isTRC10,
+    isTRC20,
+} from "./helpers";
 
 describe("Check repository root dir", () => {
     const rootDirAllowedFiles = [
+        ".github",
         "blockchains",
         "dapps",
         "media",
         "node_modules",
         "script",
         "test",
+        ".gitignore",
+        ".travis.yml",
+        "babel.config.js",
         "LICENSE",
         "package-lock.json",
         "package.json",
         "README.md",
         ".git",
-        ".github",
-        ".gitignore",
-        ".travis.yml"
     ]
-    const dirActualFiles = readDirSync(".")
 
-    test("Root should contains only predifined files", () => {
-        rootDirAllowedFiles.forEach(file => {
-            expect(dirActualFiles.indexOf(file)).not.toBe(-1)
+    const dirActualFiles = readDirSync(".")
+    test("Root should contains only predefined files", () => {
+        dirActualFiles.forEach(file => {
+            expect(rootDirAllowedFiles.indexOf(file), `File "${file}" should not be in root or added to predifined list`).not.toBe(-1)
         })
     })
 })
