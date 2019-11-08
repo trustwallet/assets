09b7fb00ffd01768f10feb757ad57a93bc02c64f
Mykola <kolya182@gmail.com>
2019-11-07 18:33:27 -0800

    script: generate white and black lists

---
+++ src/test/gen_list.ts
@@ -0,0 +1,79 @@
+const fs = require('fs')
+import { getOpenseaCollectionAddresses } from "../../script/opesea_contrats"
+
+import {
+    Ethereum,
+    getChainAssetsPath,
+    readDirSync,
+    readFileSync,
+    isChainWhitelistExistSync,
+    isChainBlacklistExistSync,
+    getChainWhitelistPath,
+    getChainBlacklistPath,
+    writeFileSync,
+    sortDesc,
+    getUnique,
+    mapList
+} from './helpers'
+
+// Current chains where assets tokens supported by Trust Wallet
+// TODO auto create list based on assets folder presence
+const chainsSupportedWhiteAndBlacklist = [
+    'binance',
+    'classic',
+    'eos',
+    Ethereum,
+    'gochain',
+    'ontology',
+    'poa',
+    'theta',
+    'thundertoken',
+    'tomochain',
+    'tron',
+    'vechain',
+    'wanchain',
+]
+
+chainsSupportedWhiteAndBlacklist.forEach(async chain => {
+    const assets = readDirSync(getChainAssetsPath(chain))
+
+    const whitelistPath = getChainWhitelistPath(chain)
+    const blacklistPath = getChainBlacklistPath(chain)
+
+    //Create inital lists if they do not exists 
+    if (!isChainWhitelistExistSync(chain)) {
+        writeFileSync(whitelistPath, `[]`)
+    }
+
+    if (!isChainBlacklistExistSync(chain)) {
+        writeFileSync(blacklistPath, `[]`)
+    }
+
+    const currentWhitelist = assets.concat(JSON.parse(readFileSync(whitelistPath)))
+    let currentBlacklist = JSON.parse(readFileSync(blacklistPath))
+
+    // Some chains required pulling lists from other sources
+    switch (chain) {
+        case Ethereum:
+            const nftList = await getOpenseaCollectionAddresses()
+            currentBlacklist = currentBlacklist.concat(nftList)
+            break;
+        default:
+            break;
+    }
+
+    const mappedWhiteList = mapList(currentWhitelist)
+    const mappedBlackList = mapList(currentBlacklist)
+
+    // Make sure whitelist do not contain assets from blacklist and oposite
+    const filteredWhitelist = currentWhitelist.filter(a => !mappedBlackList[a])
+    const filteredBlacklist = currentBlacklist.filter(a => !mappedWhiteList[a])
+    
+    const finalWhiteList = sortDesc(getUnique(filteredWhitelist))
+    const finalBlackList = sortDesc(getUnique(filteredBlacklist))
+
+    fs.writeFileSync(whitelistPath, JSON.stringify(finalWhiteList, null, 4))
+    fs.writeFileSync(blacklistPath, JSON.stringify(finalBlackList, null, 4))
+})
+
+
