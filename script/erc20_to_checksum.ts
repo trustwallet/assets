048b941bf4cd0e0434effa1710d4b4133c5860ab
Mykola <kolya182@gmail.com>
2020-01-19 21:34:25 -0800

    Move checksum script to ts

--- script/erc20_to_checksum.ts
+++ script/erc20_to_checksum.ts
@@ -3,17 +3,14 @@ const fs = require('fs')
 const path = require('path')
 const Web3 = require('web3')
 const web3 = new Web3('ws://localhost:8546');
+import { ethSidechains, readDirSync } from "../src/test/helpers"
 
-const readdirSync = path => fs.readdirSync(path)
-
-const cheinsAddressesToChecksum = ["classic", "poa", "tomochain", "gochain", "wanchain", "thundertoken", "ethereum"]
-
-cheinsAddressesToChecksum.forEach(chain => {
+ethSidechains.forEach(chain => {
     const assetsPath = path.resolve(`${__dirname}/../blockchains/${chain}/assets`) 
-    const chainAddresses = readdirSync(assetsPath)
+    const chainAddresses = readDirSync(assetsPath)
 
     chainAddresses.forEach(addr => {
-        isChecksum = web3.utils.checkAddressChecksum(addr)
+        const isChecksum = web3.utils.checkAddressChecksum(addr)
 
         if (!isChecksum) {
             console.log(`Address ${addr} not in checksum`)
