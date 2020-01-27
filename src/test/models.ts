f50171c52a892f192683a5e2d41602797e697f11
Mykola <kolya182@gmail.com>
2020-01-27 00:06:24 -0800

    Add infoList interface

--- src/test/models.ts
+++ src/test/models.ts
@@ -4,3 +4,32 @@ export interface ValidatorModel {
     description: string,
     website: string,
 }
+
+export interface InfoList {
+    name: string;
+    website: string;
+    source_code: string;
+    whitepaper: string;
+    explorers: Explorer[];
+    socials: Social[];
+    details: Detail[];
+    data_source: string;
+  }
+  
+  interface Detail {
+    language: string;
+    description: string;
+  }
+
+  interface Social {
+    name: string;
+    url: string;
+    username: string;
+  }
+  
+  interface Explorer {
+    name: string;
+    url: string;
+  }
+  
+
