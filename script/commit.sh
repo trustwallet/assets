db93070bc6a75d887d07fdd36e2d843fcaad290c
Mykola <kolya182@gmail.com>
2020-01-31 00:29:57 -0800

    log branch

--- script/commit.sh
+++ script/commit.sh
@@ -4,6 +4,7 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
     git add .
     git commit -m "Checksum ERC20 addresses [skip ci]"
     branch=$(git symbolic-ref --short -q HEAD)
+    echo "Current branch $(Build.SourceBranch)"
     echo "Pushing changes to branch: $branch"
     git push origin $branch
 else
