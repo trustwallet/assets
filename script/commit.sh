68033252f5f244818d58c51f29fb5c8d70616eaf
Mykola <kolya182@gmail.com>
2020-01-31 01:07:01 -0800

    push

--- script/commit.sh
+++ script/commit.sh
@@ -3,10 +3,10 @@
 if [[ `git status --porcelain --untracked-files=no` ]]; then
     git add .
     git commit -m "Checksum ERC20 addresses [skip ci]"
-    branch=$(git symbolic-ref --short -q HEAD)
-    echo Current branch $BUILD_SOURCEBRANCH
-    echo echoPushing changes to branch: $branch
-    git push origin HEAD:$branch
+    echo Current branch 
+    echo Pushing changes to branch: $BUILD_SOURCEBRANCH
+    git push origin HEAD:$BUILD_SOURCEBRANCH
+    echo Complete
 else
     echo "Nothing to commit"
 fi
