a2d39b106e7e7ff62b943db65eac1cedf9962a18
Mykola <kolya182@gmail.com>
2020-01-31 02:05:16 -0800

    just push

--- script/commit.sh
+++ script/commit.sh
@@ -6,7 +6,7 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
     echo Current branch 
     echo Pushing changes to branch: $BUILD_SOURCEBRANCH
     git status
-    git -c http.extraheader="AUTHORIZATION: bearer $SYSTEM_ACCESSTOKEN" push origin HEAD:$BUILD_SOURCEBRANCH
+    git push
     echo Complete
 else
     echo "Nothing to commit"
