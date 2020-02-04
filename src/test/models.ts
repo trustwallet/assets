5bf02ce2db05dca91a7175192ebf8a01260550a9
Mykola <kolya182@gmail.com>
2020-02-03 22:45:21 -0800

    Move interfaces

--- src/test/models.ts
+++ src/test/models.ts
@@ -32,4 +32,25 @@ export interface InfoList {
     url: string;
   }
   
+// CoinmarketCap
+export interface mapTiker {
+    coin: number
+    type: mapType
+    token_id?: string
+    id: number
+}
 
+export type mapType = TickerType.Coin | TickerType.Token 
+
+export enum TickerType {
+    Token = "token",
+    Coin = "coin"
+}
+
+export enum PlatformType {
+    Ethereum = "Ethereum",
+    Binance = "Binance Coin",
+    TRON = "TRON",
+    OMNI = "Omni",
+    VeChain = "VeChain"
+}
