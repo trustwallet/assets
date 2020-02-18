69c2f87efa6778aa331037dc482088e0d7b50440
Mykola <kolya182@gmail.com>
2020-02-18 14:02:16 -0800

    Use SYSTEM_PULLREQUEST_SOURCEBRANCH var to get branch name

--- script/commit.sh
+++ script/commit.sh
@@ -4,9 +4,9 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
     git add .
     git commit -m "Checksum ERC20 addresses [skip ci]"
     echo Current branch 
-    echo Pushing changes to branch: $BUILD_SOURCEBRANCH
+    echo Pushing changes to branch: $SYSTEM_PULLREQUEST_SOURCEBRANCH
     git status
-    git push origin HEAD:$BUILD_SOURCEBRANCH
+    git push origin HEAD:$SYSTEM_PULLREQUEST_SOURCEBRANCH
     echo Complete
 else
     echo "Nothing to commit"
