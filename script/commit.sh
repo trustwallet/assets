b4680ed7b2acc1269c691ed7bf7ce6c7e6b55a4a
Mykola <kolya182@gmail.com>
2020-02-18 14:45:21 -0800

    Use Bash task

--- script/commit.sh
+++ script/commit.sh
@@ -4,9 +4,10 @@ if [[ `git status --porcelain --untracked-files=no` ]]; then
     git add .
     git commit -m "Checksum ERC20 addresses [skip ci]"
     echo Current branch 
-    echo Pushing changes to branch: $SYSTEM_PULLREQUEST_SOURCEBRANCH
+    echo Pushing changes to branch: "$SYSTEM_PULLREQUEST_SOURCEBRANCH"
+    echo Current branch $(git branch)
     git status
-    git push origin HEAD:$SYSTEM_PULLREQUEST_SOURCEBRANCH
+    git push origin HEAD:"$SYSTEM_PULLREQUEST_SOURCEBRANCH"
     echo Complete
 else
     echo "Nothing to commit"
