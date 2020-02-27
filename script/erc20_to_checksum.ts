f4c8c2eb777e0db6380bf7373ca6d3016316e30e
Mykola <kolya182@gmail.com>
2020-02-26 16:10:15 -0800

    Use task in pipeline

--- script/erc20_to_checksum.ts
+++ script/erc20_to_checksum.ts
@@ -12,11 +12,11 @@ ethSidechains.forEach(chain => {
         const isChecksum = web3.utils.checkAddressChecksum(addr)
 
         if (!isChecksum) {
-            console.log(`Address ${addr} not in checksum`)
+            console.log(`Renaming non checksum ${addr} ...`)
             const checksum = web3.utils.toChecksumAddress(addr)
             const moveToChecksum = `git mv ${addr} ${checksum}-temp && git mv ${checksum}-temp ${checksum}`
             const renamed = execSync(`cd ${assetsPath} && ${moveToChecksum}`, {encoding: "utf-8"})
-            console.log(`Result renaming ${addr} : ${renamed}`)
+            console.log(`   Result renaming ${addr} : ${renamed}`)
         }
     })
 });
