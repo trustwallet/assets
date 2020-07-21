5861b2fd473e4b3d1622fed778a9e1b47fb1ccb3
Adam R <13562139+catenocrypt@users.noreply.github.com>
2020-07-21 15:29:58 +0200

    Optimization for black/whitelist checking. (#2825)
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>
    Co-authored-by: Andrew M <35627271+zachzwei@users.noreply.github.com>

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -180,6 +180,25 @@ export const mapList = arr => {
     }, {})
 }
 
+export function findDuplicate(list: string[]): string {
+    let m = new Map<string, number>()
+    let duplicate: string = null
+    list.forEach(val => {
+        if (m.has(val)) {
+            duplicate = val
+        } else {
+            m.set(val, 0)
+        }
+    })
+    return duplicate
+}
+
+// Check that two lists have no common elements, and no duplicates in either.
+// Do a single check: checking for duplicates in the concatenated list.
+export function findCommonElementOrDuplicate(list1: string[], list2: string[]) {
+    return findDuplicate(list1.concat(list2))
+}
+
 export const getImageDimentions = (path: string) => sizeOf(path)
 
 export function isLogoDimentionOK(path: string): [boolean,  string] {
