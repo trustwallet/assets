3eec83ba85faff7a8f7d8a26a96921c441fe156d
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-07-30 16:14:52 +0200

    Simplify checkStep class/instance creation.

--- script/action/folders-and-files.ts
+++ script/action/folders-and-files.ts
@@ -5,25 +5,24 @@ import { CheckStepInterface, ActionInterface } from "./interface";
 const defaultRootDirAllowedFiles = [".github", "blockchains", "dapps", "media", "script", "src", ".gitignore", "LICENSE", "package-lock.json", "package.json", "README.md", ".git", "Gemfile", "Gemfile.lock"];
 const rootDirAllowedFiles = config.getConfig("folders_rootdir_allowed_files", defaultRootDirAllowedFiles);
 
-class RootDir implements CheckStepInterface {
-    getName(): string { return "Repository root dir"; }
-    check(): string {
-        var error: string = "";
-        const dirActualFiles = readDirSync(".");
-        dirActualFiles.forEach(file => {
-            if (!(rootDirAllowedFiles.indexOf(file) >= 0)) {
-                error += `File "${file}" should not be in root or added to predifined list\n`;
-            }
-        });
-        return error;
-    }
-}
 
 export class FoldersFiles implements ActionInterface {
     getName(): string { return "Folders and Files"; }
     getChecks(): CheckStepInterface[] {
         return [
-            new RootDir()
+            {
+                getName: () => { return "Repository root dir"},
+                check: () => {
+                    var error: string = "";
+                    const dirActualFiles = readDirSync(".");
+                    dirActualFiles.forEach(file => {
+                        if (!(rootDirAllowedFiles.indexOf(file) >= 0)) {
+                            error += `File "${file}" should not be in root or added to predifined list\n`;
+                        }
+                    });
+                    return error;
+                }
+            }
         ];
     }
     fix = null;
