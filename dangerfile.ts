8659f38aee7288cd12a73ae7004cc84e61f0e922
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-08-12 10:01:31 +0200

    Finetune Danger message.

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,9 @@
-import { message, danger, markdown, warn } from "danger";
+import { fail, markdown } from "danger";
 import { sanityCheckAll } from "./script/action/update-all";
 
-sanityCheckAll().then(sanityErrors => {
-    sanityErrors.forEach(err => warn(err));
+sanityCheckAll().then(errors => {
+    if (errors.length > 0) {
+        errors.forEach(err => fail(err));
+        markdown("Please fix the errors above.  Files can be replaced/renamed in this pull request (using command-line, or GitHub Desktop).  Alternatively, you may close this pull request and open a new one.");
+    }
 });
