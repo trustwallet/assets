1f863be09257e40ab653b05fe5c8cc480185e2e2
Tican227 <76060552+Tican227@users.noreply.github.com>
2020-12-19 02:50:41 +0545

    Add files via upload

---
+++ blockchains/tron/assets/info.sol
@@ -0,0 +1,7 @@
+{
+    "name": "Tican",
+    "website": "https://www.tican.io",
+    "short_description": "Klever app is described to be a simple, secure p2p crypto wallet for blockchain.",
+    "explorer": "https://tronscan.org/#/token20/TPPJsyJVZ1ykxDDBnx2KwuUo3brF1EdR1W",
+    "research": ""
+}
