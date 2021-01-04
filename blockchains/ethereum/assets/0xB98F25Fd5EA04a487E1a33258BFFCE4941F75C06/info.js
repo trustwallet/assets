a8c808a5a10e27c814f349ae658f39673a26daac
Hiep Tran <hieptran.dev@gmail.com>
2021-01-05 06:54:18 +0700

    Add INSA and logo
    The first init INSA logo

---
+++ blockchains/ethereum/assets/0xB98F25Fd5EA04a487E1a33258BFFCE4941F75C06/info.js
@@ -0,0 +1,6 @@
+{
+  "name": "INSA (INS)",
+  "website": "https://insa.vip",
+  "short_description": "INSA is an application platform that enables online businesses to import, market and sell their goods. Besides, wholesale network building and customer services managing are also facilitated, thus become easier and more profitable.",
+  "explorer": "https://etherscan.io/token/0xb98f25fd5ea04a487e1a33258bffce4941f75c06"
+}
