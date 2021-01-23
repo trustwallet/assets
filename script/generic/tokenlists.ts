87fee8c064310d3fcbab1c9b445bd8ed1891c265
Catenocrypt <catenocrypt@users.noreply.github.com>
2021-01-23 01:01:11 +0100

    Common writeToFile method, sort.

--- script/generic/tokenlists.ts
+++ script/generic/tokenlists.ts
@@ -1,3 +1,5 @@
+import { writeJsonFile } from "../generic/json";
+
 class Version {
     major: number
     minor: number
@@ -66,7 +68,23 @@ export function generateTokensList(titleCoin: string, tokens: TokenItem[]): List
         `Trust Wallet: ${titleCoin}`,
         "https://trustwallet.com/assets/images/favicon.png",
         "2020-10-03T12:37:57.000+00:00",
-        tokens,
+        tokens.sort((t1,t2) => {
+            const t1pairs = (t1.pairs || []).length;
+            const t2pairs = (t2.pairs || []).length;
+            if (t1pairs != t2pairs) { return t2pairs - t1pairs; }
+            return t1.address.localeCompare(t2.address);
+        }),
         new Version(0, 1, 0)
     )
+}
+
+function totalPairs(list: List): number {
+    let c = 0;
+    list.tokens.forEach(t => c += (t.pairs || []).length);
+    return c;
+}
+
+export function writeToFile(filename: string, list: List): void {
+    writeJsonFile(filename, list);
+    console.log(`Tokenlist: list with ${list.tokens.length} tokens and ${totalPairs(list)} pairs written to ${filename}.`);
 }
