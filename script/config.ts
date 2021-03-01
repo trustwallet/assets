c8f7520574668c057970087b4a15fdd0ba2e3b85
Adam R <13562139+catenocrypt@users.noreply.github.com>
2021-03-01 16:02:06 +0100

    Update pacakeswap API provider. (#5766)
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>

--- script/config.ts
+++ script/config.ts
@@ -11,7 +11,7 @@ export const assetsURL = 'https://raw.githubusercontent.com/trustwallet/assets/m
 // Force include & exclude config: list of token symbols, or symbol pairs (e.g. ["Cake", "DAI-WBNB"]).
 export const PancakeSwap_ForceInclude: string[] = ["Cake", "DAI", "ETH", "TWT", "VAI", "USDT", "BLINK", "BTCB", "ALPHA", "INJ", "CTK", "UNI", "XVS", "BUSD", "HARD", "BIFI", "FRONT"];
 export const PancakeSwap_ForceExclude: string[] = [];
-export const PancakeSwap_TradingPairsUrl = "https://api.bscgraph.org/subgraphs/name/wowswap";
+export const PancakeSwap_TradingPairsUrl = "https://api.bscgraph.org/subgraphs/name/cakeswap";
 export const PancakeSwap_TradingPairsQuery = `
     query pairs {
         pairs(first: 150, orderBy: reserveUSD, orderDirection: desc) {
