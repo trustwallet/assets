eb6211ea1e00b1de289251b5feb0d8439c02dfb5
rbscfinance <79531871+rbscfinance@users.noreply.github.com>
2021-03-01 19:49:53 +0200

    Update info.sol

--- blockchains/smartchain/assets/0xffAC0Fc920b487179354D8CDF9D8674dcF247495/info.sol
+++ blockchains/smartchain/assets/0xffAC0Fc920b487179354D8CDF9D8674dcF247495/info.sol
@@ -1,11 +1,23 @@
 {
     "name": "rBSC Token Yield",
     "website": "https://rbsc.financial",
-    "twitter": "https://twitter.com/rbscfinance",
-    "telegram": "https://t.me/rbsc_token_community",
-    "description": "rBSC is a token on the Binance Smart Chain. rBSC works by applying a 2% fee to each transaction and instantly splitting that fee among all holders of the token. Holders do not need to stake or wait for fees to be delivered. Fees are awarded by the smart contract and are immediately reflected in the holders balance.",
-    "explorer": "https://bscscan.com/token/0xffac0fc920b487179354d8cdf9d8674dcf247495",
     "source_code": "https://github.com/rbscfinance",
+    "description": "rBSC is a token on the Binance Smart Chain. rBSC works by applying a 2% fee to each transaction and instantly splitting that fee among all holders of the token. Holders do not need to stake or wait for fees to be delivered. Fees are awarded by the smart contract and are immediately reflected in the holders balance.",
+    "socials": [
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/rbscfinance"
+        },
+        {
+            "name": "Medium",
+            "url": "https://rbscfinancial.medium.com"
+        },
+        {
+            "name": "Telegram",
+            "url": "https://t.me/rbsc_token_community"
+        }
+    ],
+    "explorer": "https://bscscan.com/token/0xffac0fc920b487179354d8cdf9d8674dcf247495",
     "type": "BEP20",
     "symbol": "rBSC",
     "decimals": 9,
