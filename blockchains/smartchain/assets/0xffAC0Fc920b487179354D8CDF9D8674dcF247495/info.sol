29636f757c63a05216fe415b37666a2c8cff0268
rbscfinance <79531871+rbscfinance@users.noreply.github.com>
2021-03-01 18:25:18 +0200

    Add rBSC logo and info
    This is adding information and a token logo

---
+++ blockchains/smartchain/assets/0xffAC0Fc920b487179354D8CDF9D8674dcF247495/info.sol
@@ -0,0 +1,14 @@
+{
+    "name": "rBSC Token Yield",
+    "website": "https://rbsc.financial",
+    "twitter": "https://twitter.com/rbscfinance",
+    "telegram": "https://t.me/rbsc_token_community",
+    "description": "rBSC is a token on the Binance Smart Chain. rBSC works by applying a 2% fee to each transaction and instantly splitting that fee among all holders of the token. Holders do not need to stake or wait for fees to be delivered. Fees are awarded by the smart contract and are immediately reflected in the holders balance.",
+    "explorer": "https://bscscan.com/token/0xffac0fc920b487179354d8cdf9d8674dcf247495",
+    "source_code": "https://github.com/rbscfinance",
+    "type": "BEP20",
+    "symbol": "rBSC",
+    "decimals": 9,
+    "status": "active",
+    "id": "0xffac0fc920b487179354d8cdf9d8674dcf247495"
+}
