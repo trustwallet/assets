cf59b9695ef49e20e0d4e687663c4bf6a4687429
CryptoA <82717856+Cryptocool1@users.noreply.github.com>
2021-05-29 21:23:50 +0300

    Update info.json.sol

--- blockchains/ethereum/assets/0x303de4bdb189b951f875eb4a8ecde2985138161e/info.json.sol
+++ blockchains/ethereum/assets/0x303de4bdb189b951f875eb4a8ecde2985138161e/info.json.sol
@@ -1,4 +1,5 @@
-    "name": "USD SMART",
+{   
+   "name": "USD SMART",
     "website": "https://usdsmart.com/",
     "description": "USDs is a Digital assets for a stable currency with Binance Smart Chain Network #BSC For Smart Project.",
     "explorer": "https://bscscan.com/token/0x303de4bdb189b951f875eb4a8ecde2985138161e",
