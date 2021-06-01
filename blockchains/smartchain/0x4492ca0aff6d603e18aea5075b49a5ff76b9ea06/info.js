7849f8379806fea06adc03bb64f0cb8b0afecceb
leafsfan <toolazy85@yahoo.ca>
2021-06-01 00:42:07 -0400

    Add files via upload

---
+++ blockchains/smartchain/0x4492ca0aff6d603e18aea5075b49a5ff76b9ea06/info.js
@@ -0,0 +1,11 @@
+{
+"name": "Farmageddon Token",
+"website": "https://farmageddon-token.com/",
+"description": "FARMING MADE EASY. Our platform is all about bringing crypto yield farming to the masses. The high ROIs are very attractive, but the confusion is real! Farmageddon will be a one-stop shop for all things crypto farming - simplifying and sorting through all the confusion for you.",
+"explorer": "https://bscscan.com/token/0x4492ca0aff6d603e18aea5075b49a5ff76b9ea06",
+"type": "BEP20",
+"symbol": "FG",
+"decimals": 9,
+"status": "active",
+"id": "0x4492ca0aff6d603e18aea5075b49a5ff76b9ea06"
+}
