cab9e76c204fca8b595c61fe9000c8e9c3ab81eb
Flutecoin <85007522+flutecoin@users.noreply.github.com>
2021-06-15 12:26:10 +0530

    Add FLUTE COIN (BEP20)

---
+++ 0x814cC7fD0fB14d5581E1c9C1b9719cBBaCB246d7/info.js
@@ -0,0 +1,10 @@
+ "name": "FLUTE COIN",
+    "website": "https://flutecoin.club",
+    "description": "Flute is one and only DeFi NFT Music Platform.",
+    "explorer": "https://bscscan.com/token/0x814cc7fd0fb14d5581e1c9c1b9719cbbacb246d7/",
+    "type": "BEP20",
+    "symbol": "FLUTE",
+    "decimals": 0,
+    "status": "active",
+    "id": "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
+}
