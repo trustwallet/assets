beae32d0aef49ff5b42ad9aaca1691c6de7f6c40
Black Phoenix <officialBlackPhoenix@outlook.com>
2021-07-06 01:58:15 +0430

    Update info.json.sol

--- blockchains/tron/assets/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET/info.json.sol
+++ blockchains/tron/assets/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET/info.json.sol
@@ -1,15 +1,14 @@
   {
     "name": "Black Phoenix",
-    "type": "TRC20",
-    "symbol": "BPX",
-    "decimals": 18,
     "website": "https://www.blackphoenixBPX.com",
     "description":Black Phoenix is a cryptocurrency with bright future ahead ,that bring benefits more and more for its holders and wants to make you a major investor in digital currency market.", 
     "explorer":"https://tronscan.io/#/token20/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET",
+    "type": "TRC20",
+    "symbol": "BPX",
+    "decimals": 18,
     "status": "active",
     "id": "TXBcx59eDVndV5upFQnTR2xdvqFd5reXET",
     "links": [
-    
         {  "name": "coinmarketcap"
            "url": "https://coinmarketcap.com/currencies/black-phoenix/"
         },
