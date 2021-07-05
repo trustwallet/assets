87f5b12ae04596c1822a795b7f118fea8f1a214d
Black Phoenix <officialBlackPhoenix@outlook.com>
2021-07-06 01:47:45 +0430

    update info.json.sol

--- blockchains/tron/assets/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET/info.json.sol
+++ blockchains/tron/assets/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET/info.json.sol
@@ -1,34 +1,37 @@
-1  {
-2    "name": "Black Phoenix",
-3    "type": "TRC20",
-4    "symbol": "BPX",
-5    "decimals": 18,
-6    "website": "https://www.blackphoenixBPX.com",
-7    "description":Black Phoenix is a cryptocurrency with bright future ahead ,that bring benefits more and more for its holders and wants to make you a major investor in digital currency market.", 
-8    "explorer":"https://tronscan.io/#/token20/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET".
-9    "status": "active",
-10    "id": "TXBcx59eDVndV5upFQnTR2xdvqFd5reXET",
-11    "links": 12[
-12        }
-13             "name": "coinmarketcap",
-14            "url": "https://coinmarketcap.com/currencies/black-phoenix/"
-15        },
-16        {
-17            "name": "github",
-18            "url": "https://github.com/BLACKPHOENIXBPX"
-19        },
-20        {
-21            "name": "twitter",
-22            "url": "https://twitter.com/OfficialBPX"
-23        },
-24        {
-25            "name": "telegram",
-26            "url": "https://t.me/BlackPhoenixNEWS"
-17        },
-28        {
-29            "name": "reddit",
-30            "url": "https://www.reddit.com/u/OfficialBlackPhoenix"
-31        }
-32        {
-33            "name": "whitepaper",
-34            "url": "https://blackphoenixbpx.com/wp-content/uploads/2021/06/BLACK_PHOENIX_Black.Paperr1.pdf" 
+  {
+    "name": "Black Phoenix",
+    "type": "TRC20",
+    "symbol": "BPX",
+    "decimals": 18,
+    "website": "https://www.blackphoenixBPX.com",
+    "description":Black Phoenix is a cryptocurrency with bright future ahead ,that bring benefits more and more for its holders and wants to make you a major investor in digital currency market.", 
+    "explorer":"https://tronscan.io/#/token20/TXBcx59eDVndV5upFQnTR2xdvqFd5reXET",
+    "status": "active",
+    "id": "TXBcx59eDVndV5upFQnTR2xdvqFd5reXET",
+    "links": [
+    
+        {  "name": "coinmarketcap"
+           "url": "https://coinmarketcap.com/currencies/black-phoenix/"
+        },
+        {
+           "name": "github"
+            "url": "https://github.com/BLACKPHOENIXBPX"
+        },
+        {
+            "name": "twitter"
+            "url": "https://twitter.com/OfficialBPX"
+        },
+        {
+            "name": "telegram"
+            "url": "https://t.me/BlackPhoenixNEWS"
+        },
+        {
+            "name": "reddit"
+            "url": "https://www.reddit.com/u/OfficialBlackPhoenix"
+        },
+        {
+            "name": "whitepaper",
+          "url": "https://blackphoenixbpx.com/wp-content/uploads/2021/06/BLACK_PHOENIX_Black.Paperr1.pdf" 
+       {
+     ]  
+  }    
