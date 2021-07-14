9b0360cb3b4f63daf80880df6602e2b98a64df98
Adam R <13562139+catenocrypt@users.noreply.github.com>
2021-07-14 09:42:21 +0200

    [Internal] Add info.json to some Classic tokens where missing (5) (#11740)
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>

--- script/generic/folders-and-files.ts
+++ script/generic/folders-and-files.ts
@@ -93,7 +93,7 @@ export class FoldersFiles implements ActionInterface {
                                     if (!infoExists && logoExists) {
                                         const msg = `Missing info file for asset '${chain}/${address}' -- ${infoFullPath}`;
                                         // enforce that info must be present (with some exceptions)
-                                        if (chain === 'classic' || chain === 'terra') {
+                                        if (chain === 'terra') {
                                             //console.log(msg);
                                             warnings.push(msg);
                                         } else {
