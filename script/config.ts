e0b2ded3cbe8ec25e9935383eeeb6ab31c72c006
Daniel <24758309+leedaniil@users.noreply.github.com>
2021-11-22 23:40:18 +0300

    Add allowed files (#16003)

--- script/config.ts
+++ script/config.ts
@@ -3,7 +3,7 @@ export const imageMaxLogoHeight =  512;
 export const imageMinLogoWidth =  64;
 export const imageMinLogoHeight =  64;
 export const imageMaxLogoSizeKb =  100;
-export const foldersRootdirAllowedFiles: string[] =  [".github", "blockchains", "dapps", "media", "node_modules", "script-old", "script", "test", ".gitignore", "azure-pipelines.yml", "jest.config.js", "LICENSE", "package-lock.json", "package.json", "README.md", ".git", ".eslintignore", ".eslintrc.js"];
+export const foldersRootdirAllowedFiles: string[] =  [".github", "blockchains", "dapps", "media", "node_modules", "script-old", "script", "test", ".gitignore", "azure-pipelines.yml", "jest.config.js", "LICENSE", "package-lock.json", "package.json", "README.md", ".git", ".eslintignore", ".eslintrc.js", "cmd", "go.mod", "go.sum"];
 export const binanceUrlTokenAssets =  "https://explorer.binance.org/api/v1/assets?page=1&rows=1000";
 export const binanceDexURL = 'https://dex-atlantic.binance.org/api'
 export const assetsURL = 'https://assets.trustwalletapp.com'
