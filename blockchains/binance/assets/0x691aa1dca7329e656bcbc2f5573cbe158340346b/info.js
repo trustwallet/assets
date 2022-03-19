972b06b006009554d71d21e46814fa2e2a9b8a69
ExpertDoge <101358789+EXPERTDOGE@users.noreply.github.com>
2022-03-19 15:50:14 +0300

    Add EDoge logo and info
    For update logo and info

---
+++ blockchains/binance/assets/0x691aa1dca7329e656bcbc2f5573cbe158340346b/info.js
@@ -0,0 +1,10 @@
+    "name": "EXPERT DOGE",
+    "website": "https://expertdogebsc.com",
+    "description": "We want to make this project as transparent and fair as possible for every investor.",
+    "explorer": "https://bscscan.com/token/0x691aa1dca7329e656bcbc2f5573cbe158340346b",
+    "type": "BEP20",
+    "symbol": "EDoge",
+    "decimals": 9,
+    "status": "active",
+    "id": "0x691aa1dca7329e656bcbc2f5573cbe158340346b"
+}
