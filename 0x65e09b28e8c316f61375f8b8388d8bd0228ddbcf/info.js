2131d0bc9ae4a0c5aae257851b764042d8150df5
arthurarjun <102993047+arthurarjun@users.noreply.github.com>
2022-04-05 02:14:31 +0700

    Add DGT logo and info

---
+++ 0x65e09b28e8c316f61375f8b8388d8bd0228ddbcf/info.js
@@ -0,0 +1,11 @@
+{
+    "name": "Digital Gold Token",
+    "type": "BEP20",
+    "symbol": "DGT",
+    "decimals": 18,
+    "website": "https://dgitoken.com/",
+    "description": "Digital Gold Token will turn gold and crypto into an ecosystem that interacts with each other as transactions or investments.",
+    "explorer": "https://bscscan.com/token/0x65e09b28e8c316f61375f8b8388d8bd0228ddbcf",
+    "status": "active",
+    "id": "0x65e09b28e8c316f61375f8b8388d8bd0228ddbcf"
+}
