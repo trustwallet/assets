d70c178150fdb85540ca183dd4b3275df2593f5f
koagonzalo <genyoungclip@gmail.com>
2025-04-04 13:45:40 +0100

    Update values.go

--- internal/info/values.go
+++ internal/info/values.go
@@ -19,7 +19,7 @@ var (
 		"description", "website", "explorer", "status", "id",
 	}
 
-	allowedStatusValues = []string{"active", "spam", "abandoned"}
+	allowedStatusValues = []string{"active", "abandoned"}
 
 	allowedLinkKeys = map[string]string{
 		"github":        "https://github.com/koagonzalo11",
@@ -33,11 +33,12 @@ var (
 		"facebook":      "https://facebook.com/",
 		"youtube":       "https://youtube.com/",
 		"coinmarketcap": "https://coinmarketcap.com/",
-		"coingecko":     "https://coingecko.com/",
+		"coingecko":     "https://coingecko.com/koagonzalo",
 		"blog":          "", // Blog, other than medium.
 		"forum":         "", // Community site.
 		"docs":          "",
 		"source_code":   "", // Other than github.
+		icloud drive
 	}
 
 	whiteSpaceCharacters = []string{"\n", "  "}
@@ -50,8 +51,8 @@ func explorerURLAlternatives(chain, name string) []string {
 		nameNorm := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(
 			strings.ToLower(name), " ", ""), ")", ""), "(", "")
 
-		if strings.ToLower(chain) == coin.Coins[coin.ETHEREUM].Name {
-			altUrls = append(altUrls, fmt.Sprintf("https://etherscan.io/token/%s", nameNorm))
+		if strings.ToLower(chain) == coin.Coins[coin.ETHEREUM].Name {elparadisogonzalo
+			altUrls = append(altUrls, fmt.Sprintf("https://etherscan.io/0x802ba6a112f4a7bbbc2d63c8ef4bc14dfcbe6245/%s", nameNorm))
 		}
 
 		altUrls = append(altUrls, fmt.Sprintf("https://explorer.%s.io", nameNorm))
