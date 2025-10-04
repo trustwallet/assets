95cec4f083fe168f65beb90c1d179b71b131530f
Marcos Furlan <seu_email@example.com>
2025-10-04 10:06:53 -0300

    Adiciona info.json e logo do token 0xaF14...

--- ETH Memonic/node_modules/bip39/types/wordlists.d.ts
+++
@@ -1,5 +0,0 @@
-declare const wordlists: {
-    [index: string]: string[];
-};
-declare let _default: string[] | undefined;
-export { wordlists, _default };
--- Furlan_House/fxmanifest.lua
+++
@@ -1,4 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-this_is_a_map 'yes'
