adc81d10f205cd8828adc15e40509a5ddac4b86a
kaixuanskill <69313169+kaixuanskill@users.noreply.github.com>
2025-11-06 17:32:57 +0800

    Create info.js

---
+++ blockchains/ethereum/assets/0x0dB4daDAa010493cB49AC73D7911B671fb7B7311/info.js
@@ -0,0 +1,11 @@
+{
+  "name": "Genius Coffee Token",
+  "symbol": "GCT",
+  "type": "ERC20",
+  "decimals": 18,
+  "description": "Genius Coffeee Token is an ERC-20 token to power a revolutionary Coffee Logistic Platform",
+  "website": "https://coffee.geniusdevelopment.com/",
+  "explorer": "https://etherscan.io/token/0x0db4dadaa010493cb49ac73d7911b671fb7b7311",
+  "status": "active",
+  "id": "0x0dB4daDAa010493cB49AC73D7911B671fb7B7311"
+}
